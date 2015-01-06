7d40c840a3a6b052d4a762cdb2b7fab406fdbba6
Wolfgang von Zeus <wolf@zeus.olymp.sin>
2015-01-06 05:24:32 +0100

    be verbose

--- minify_js.sh
+++ minify_js.sh
@@ -5,4 +5,4 @@ JQUERY_JS="${P}/jquery-2.1.3/jquery-2.1.3.min.js"
 JQUERY_UI_JS="${P}/jquery-ui-1.11.2/jquery-ui.js"
 TARGET_JS="${P}/shared-static.js"
 
-java -jar "${YUICOMPRESSOR_JAR}" "${JQUERY_JS}" "${JQUERY_UI_JS}" -o "${TARGET_JS}" --charset utf-8
+java -jar "${YUICOMPRESSOR_JAR}" "${JQUERY_JS}" "${JQUERY_UI_JS}" -o "${TARGET_JS}" --charset utf-8 -v
