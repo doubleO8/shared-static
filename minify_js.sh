da290c9b2859cf7cba84d03d186d118e20a0cfb4
Wolfgang von Zeus <wolf@zeus.olymp.sin>
2015-01-06 05:36:49 +0100

    fix: minifying procedure

--- minify_js.sh
+++ minify_js.sh
@@ -4,5 +4,13 @@ YUICOMPRESSOR_JAR="yuicompressor/build/yuicompressor-2.4.8.jar"
 JQUERY_JS="${P}/jquery-2.1.3/jquery-2.1.3.min.js"
 JQUERY_UI_JS="${P}/jquery-ui-1.11.2/jquery-ui.js"
 TARGET_JS="${P}/shared-static.js"
+TMP_TARGET="/tmp/minified.js"
 
-java -jar "${YUICOMPRESSOR_JAR}" "${JQUERY_JS}" "${JQUERY_UI_JS}" -o "${TARGET_JS}" --charset utf-8 -v
+java -jar "${YUICOMPRESSOR_JAR}" "${JQUERY_JS}" -o "${JQUERY_JS}.minified" --charset utf-8 -v
+java -jar "${YUICOMPRESSOR_JAR}" "${JQUERY_UI_JS}" -o "${JQUERY_UI_JS}.minified" --charset utf-8 -v
+
+cat "${JQUERY_JS}.minified" > ${TMP_TARGET}
+cat "${JQUERY_UI_JS}.minified" >> ${TMP_TARGET}
+
+mv ${TMP_TARGET} ${TARGET_JS}
+
