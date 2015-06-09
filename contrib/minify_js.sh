c2aefc6985ba3f1081b47452cbc5b290277a6d50
Wolf <wolf@hades.olymp.sin>
2015-06-09 19:21:46 +0200

    adapted path

--- contrib/minify_js.sh
+++ contrib/minify_js.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-P="public_html"
-YUICOMPRESSOR_JAR="yuicompressor/build/yuicompressor-2.4.8.jar"
+P=".."
+YUICOMPRESSOR_JAR="../yuicompressor/build/yuicompressor-2.4.8.jar"
 JQUERY_JS="${P}/jquery-2.1.3/jquery-2.1.3.min.js"
 JQUERY_UI_JS="${P}/jquery-ui-1.11.2/jquery-ui.js"
 TARGET_JS="${P}/shared-static.js"
