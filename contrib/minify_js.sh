26df7c684800cf8b4605e561a35590e31239b499
Wolf <wolf@hades.olymp.sin>
2015-06-09 20:09:17 +0200

    using the current font-awesome, jQuery and jQuery UI libraries

--- contrib/minify_js.sh
+++ contrib/minify_js.sh
@@ -2,8 +2,8 @@
 P=".."
 YUICOMPRESSOR_JAR="../yuicompressor/build/yuicompressor-2.4.8.jar"
 
-JQUERY_JS="${P}/jquery-2.1.3/jquery-2.1.3.min.js"
-JQUERY_UI_JS="${P}/jquery-ui-1.11.2/jquery-ui.js"
+JQUERY_JS="${P}/jquery-2.1.4/jquery-2.1.4.min.js"
+JQUERY_UI_JS="${P}/jquery-ui-1.11.4/jquery-ui.js"
 TARGET_JS="${P}/shared-static.js"
 TMP_TARGET="/tmp/minified.js"
 
