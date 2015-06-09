62d1a54ecabecb599b3730d2a645a7704f3fab70
Wolf <wolf@hades.olymp.sin>
2015-06-09 19:34:13 +0200

    minor cleanup

--- contrib/minify_js.sh
+++ contrib/minify_js.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 P=".."
 YUICOMPRESSOR_JAR="../yuicompressor/build/yuicompressor-2.4.8.jar"
+
 JQUERY_JS="${P}/jquery-2.1.3/jquery-2.1.3.min.js"
 JQUERY_UI_JS="${P}/jquery-ui-1.11.2/jquery-ui.js"
 TARGET_JS="${P}/shared-static.js"
